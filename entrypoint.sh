b4573c1b09569b97209569ea7b7cfd2798d04aa7
Julia Brunenberg <julia@jjim.de>
2026-06-24 13:45:03 +0200

    add variables and options for client verification via admission endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,6 @@ exec /app/derper \
     -stun="${DERP_STUN}" \
     -stun-port="${DERP_STUN_PORT}" \
     -http-port="${DERP_HTTP_PORT}" \
-    -verify-clients="${DERP_VERIFY_CLIENTS}"
+    -verify-clients="${DERP_VERIFY_CLIENTS}" \
+    -verify-client-url="${DERP_VERIFY_CLIENT_URL}" \
+    -verify-client-url-fail-open="${DERP_VERIFY_CLIENT_URL_FAILOPEN}"
